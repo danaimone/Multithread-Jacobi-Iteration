7d7fe1f6651279f4675937cdac6d0b72a3084b23
danaimone <danaimone@gmail.com>
2019-11-20 15:29:56 -0800

    init file commit

---
+++ jacobi/main.c
@@ -0,0 +1,19 @@
+/* Corben Roszak & Dan Aimone
+ * CSCI 347
+ * Jacobi Matrix Multi Threaded Project
+ *
+ * 1.0
+ */
+
+#include <stdio.h>
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+    return 0;
+}
